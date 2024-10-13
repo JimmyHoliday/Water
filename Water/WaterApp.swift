963758147e520df2371c733bf42f8d81687a2ce6
John Haverkate <john@haverkate.com>
2024-10-13 20:16:17 +0200

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by John Haverkate on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterTests/WaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTests.swift
+//  WaterTests
+//
+//  Created by John Haverkate on 13/10/2024.
+//
+
+import Testing
+@testable import Water
+
+struct WaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
